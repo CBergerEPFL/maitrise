546a5cdc04d622fe2f56196186e068979decec2f
CBergerEPFL <charlesberger34@gmail.com>
2022-11-02 17:03:16 +0000

    TSD is now jitted (numba Added) + Notebook with the jitted TSD function

--- src/shared_utils/TSD_cal.py
+++ src/shared_utils/TSD_cal.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import desolver.backend as D
-import hfda
 from math import isnan
 from numba import njit,prange
 
@@ -17,100 +16,103 @@ def system_coordinates_reader(Path_to_data, Attractor_name, num_attractor=0):
     return xyzs, t
 
 
-def Mean_taux(signal, taux, hprime, h=0):
-    N = len(signal)
-    if taux + hprime + h < N and taux + h < N - 1:
-        return np.mean((signal[int(h + taux) : int(taux + hprime + h)]))
-    elif taux + hprime + h > N and taux + h < N - 1:
-        return np.mean((signal[int(h + taux) : -1]))
-    else:
-        return 0
+@njit
+def taux_Mean_fast(signal, taux, hprime, h=0):
+    return np.mean((signal[int(h + taux) : int(taux + hprime + h)]))
 
+@njit
+def taux_var_fast(signal, taux, hprime, h=0):
+    return np.var(signal[int(h + taux) : int(taux + hprime + h)])
 
-def Variance_taux(signal, taux, hprime, h=0):
+@njit(parallel = True)
 
-    N = len(signal)
-    if taux + hprime + h <= N and taux + h < N - 1:
-        # return (1/hprime)*np.sum((signal[int(h+taux):int(taux+hprime+h)]-Mean_taux(signal,taux,hprime,h))**2)
-        return np.var(signal[int(h + taux) : int(taux + hprime + h)])
-    elif taux + hprime + h > N and taux + h < N - 1:
-        # return (1/hprime)*np.sum((signal[int(taux):-1]-Mean_taux(signal,taux,hprime,0))**2)
-        return np.var(signal[int(taux) : -1])
-    else:
-        return 0
-
-
-def I1(c, signal, fs, h, hprime, step_c, t0=0):
-    tab = np.array([],dtype = np.float64)
-    for l in c:
+def adapted_c(c_val,fs,h,hprime,signal):
+    for l in c_val:
         if (
             l * fs + hprime * len(signal) + h * len(signal) > len(signal)
             and l * fs + h * len(signal) > len(signal) - 1
         ):
-            c = c[c < l]
+            c = c_val[c_val < l]
             break
-        elif len(tab) == 0:
+    return c
+
+
+@njit
+def I1(c, signal, fs, h, hprime, step_c, t0=0):
+    tab = np.zeros_like(c)
+    for count in range(len(tab)):
+        if count ==0:
             I1c = (
                 (1 / (h * len(signal)))
                 * step_c
                 * np.abs(
-                    Mean_taux(signal, t0 * fs, hprime * len(signal), h * len(signal))
-                    - Mean_taux(signal, t0 * fs, hprime * len(signal))
+                    taux_Mean_fast(signal, t0 * fs, hprime * len(signal), h * len(signal))
+                    - taux_Mean_fast(signal, t0 * fs, hprime * len(signal))
                 )
             )
-            tab = np.append(tab,I1c)
-        else:
-            I1c = tab[-1]
-            I1c += (
+            tab[count] = I1c
+        else :
+            I1c = tab[count-1]
+            I1c = I1c + (
                 (1 / (h * len(signal)))
                 * step_c
                 * np.abs(
-                    Mean_taux(signal, l * fs, hprime * len(signal), h * len(signal))
-                    - Mean_taux(signal, l * fs, hprime * len(signal))
+                    taux_Mean_fast(signal, t0 * fs, hprime * len(signal), h * len(signal))
+                    - taux_Mean_fast(signal, t0 * fs, hprime * len(signal))
                 )
             )
-            tab = np.append(tab,I1c)
+            tab[count-1] = I1c
+    return tab[:-1]
 
-    return tab
-
-
+@njit
 def I2(c, signal, fs, h, hprime, step_c, t0=0):
-    tab = np.array([],dtype = np.float64)
-    for l in c:
-        if (
-            l * fs + hprime * len(signal) + h * len(signal) > len(signal)
-            and l * fs + h * len(signal) > len(signal) - 1
-        ):
-            c = c[c < l]
-            break
-        elif len(tab) == 0:
-            I2c = (
+    tab = np.zeros_like(c)
+    for count in range(len(tab)):
+        if count ==0:
+            I1c = (
                 (1 / (h * len(signal)))
                 * step_c
                 * np.abs(
-                    Variance_taux(signal, t0 * fs, hprime * len(signal), h * len(signal))
-                    - Variance_taux(signal, t0 * fs, hprime * len(signal))
+                    taux_var_fast(signal, t0 * fs, hprime * len(signal), h * len(signal))
+                    - taux_var_fast(signal, t0 * fs, hprime * len(signal))
                 )
             )
-            tab = np.append(tab,I2c)
-        else:
-            I2c = tab[-1]
-            I2c +=(
+            tab[count] = I1c
+        else :
+            I1c = tab[count-1]
+            I1c = I1c + (
                 (1 / (h * len(signal)))
                 * step_c
                 * np.abs(
-                    Variance_taux(signal, l * fs, hprime * len(signal), h * len(signal))
-                    - Variance_taux(signal, l * fs, hprime * len(signal))
-                ))
-            tab = np.append(tab,I2c)
-    return tab,c
+                    taux_var_fast(signal, t0 * fs, hprime * len(signal), h * len(signal))
+                    - taux_var_fast(signal, t0 * fs, hprime * len(signal))
+                )
+            )
+            tab[count-1] = I1c
+    return tab[:-1]
 
 
-def discrepancies_mean_curve(signal_tot, fs, h, hprime, step, t0=0):
-    c1 = np.arange(t0, (len(signal_tot) / fs) + t0, step)
-    I1_t = I1(c1, signal_tot, fs, h, hprime, step, t0)
-    I2_t,c = I2(c1, signal_tot, fs, h, hprime, step, t0)
-    return I1_t, I2_t, c
+@njit
+def discrepancies_mean_curve(signal_tot, fs, h, hprime,t0 = 0):
+    c1 = np.linspace(t0, int((len(signal_tot) / fs) + t0), len(signal_tot))
+    c_adapted = adapted_c(c1,fs,h,hprime,signal_tot)
+    I1_t = I1(c_adapted, signal_tot, fs, h, hprime, 1/fs, t0)
+    I2_t = I2(c_adapted, signal_tot, fs, h, hprime, 1/fs, t0)
+    return I1_t, I2_t, c_adapted
+
+
+@njit
+def Interval_calculator_lead(signal, fs, t0=0):
+    h = 0.001
+    hprime = 0.005
+    I1c, I2c, c = discrepancies_mean_curve(signal, fs, h, hprime, t0)
+    c1 = c[I1c[~np.isnan(I1c)]==np.max(I1c[~np.isnan(I1c)]) / 2]
+    c2 = c[I2c[~np.isnan(I2c)]==np.max(I2c[~np.isnan(I2c)]) / 2]
+    cs = np.minimum(np.mean(c1), np.mean(c2))
+    dic_segment_lead = (cs - t0) * fs
+    if isnan(dic_segment_lead) or dic_segment_lead<100 :
+        dic_segment_lead = 2500
+    return dic_segment_lead
 
 
 def Interval_calculator_all(dico_signal, name_signal, fs):
@@ -120,17 +122,7 @@ def Interval_calculator_all(dico_signal, name_signal, fs):
     return dic_segment_lead
 
 
-def Interval_calculator_lead(signal, fs, t0=0):
-    h = 0.001
-    hprime = 0.005
-    I1c, I2c, c = discrepancies_mean_curve(signal, fs, h, hprime, 1 / fs, t0)
-    c1 = c[np.isclose(I1c[~np.isnan(I1c)],np.max(I1c[~np.isnan(I1c)]) / 2,rtol = 0.001)]
-    c2 = c[np.isclose(I2c[~np.isnan(I2c)],np.max(I2c[~np.isnan(I2c)]) / 2,rtol = 0.001)]
-    cs = np.minimum(np.mean(c1), np.mean(c2))
-    dic_segment_lead = (cs - t0) * fs
-    if isnan(dic_segment_lead) or dic_segment_lead<100 :
-        dic_segment_lead = 2500
-    return dic_segment_lead
+
 
 
 def is_segment_flatline(sig):
@@ -140,7 +132,7 @@ def is_segment_flatline(sig):
     return True
 
 
-def TSD_index(dico_signal, name_lead, fs,k1=1):
+def TSD_index(dico_signal, name_lead, fs):
 
     ###Index Creation :TSD
     ###The label will be as follow : mean(TSD) < 1.25 = Acceptable;mean(SDR of all lead) >1.25 = Unacceptable
@@ -156,7 +148,7 @@ def TSD_index(dico_signal, name_lead, fs,k1=1):
             dico_D[i] = (2,dico_signal[i])
             D_arr = np.append(D_arr,2)
         else :
-            Dv, _ = TSD_mean_calculator(dico_signal[i],dic_segment[i],fs,k1)
+            Dv, _ = TSD_mean_calculator(dico_signal[i],dic_segment[i],fs)
             dico_D[i] = (Dv, dico_signal[i])
             D_arr = np.append(D_arr, Dv)
     return dico_D, np.mean(D_arr)
@@ -165,28 +157,34 @@ def TSD_index(dico_signal, name_lead, fs,k1=1):
 
 
 
-def TSD_index_lead(signal, segment,fs,k1=1, t0=0):
+def TSD_index_lead(signal, segment,fs):
 
     ###Index Creation :TSD for 1 lead
     ###The label will be as follow : mean(TSD) < 1.25 = Acceptable;mean(SDR of all lead) >1.25 = Unacceptable
     ##For each lead, we will return a more precise classification based on the folloying rules:
     ## TSD<1.25 = Good quality ; 1.25<TSD<1.40 = Medium quality; TSD>1.4 = Bad quality
     # dico_seg = Interval_calculator(dico_signal,name_lead,fs,t0)
-    Dv, _ = TSD_mean_calculator(signal, segment,fs,k1)
+    Dv, _ = TSD_mean_calculator(signal, segment,fs)
     return Dv
 
-def Lm_q(signal, m, k, fs):
-    N = len(signal)
-    n = np.floor((N - m) / k).astype(np.int64)
+@njit
+def Lm_q(signal1, m, k, fs):
+    N = len(signal1)
+    n = np.floor((N - m) / k)
     norm = (N - 1) / (n * k * (1 / fs))
-    sum = np.sum(np.abs(np.diff(signal[m::k], n=1)))
-    Lmq = (sum * norm) / k
+    #sum = np.sum(np.abs(np.diff(signal1[m::k], n=1)))
+    sum1 = 0
+    for i in range(1,n):
+        sum1 = sum1 + np.absolute(signal1[m+i*k]-signal1[m+(i-1)*k])
+    Lmq = (sum1 * norm) / k
     return Lmq
-
-
+@njit
 def Lq_k(signal, k, fs):
-    calc_L_series = np.frompyfunc(lambda m: Lm_q(signal, m, k, fs), 1, 1)
-    L_average = np.average(calc_L_series(np.arange(1, k + 1)))
+    #calc_L_series = np.frompyfunc(lambda m: Lm_q(signal, m, k, fs), 1, 1)
+    calc_L_series = np.zeros(k)
+    for m in range(1,k+1):
+        calc_L_series[m-1] = Lm_q(signal,m,k,fs)
+    L_average = np.mean(calc_L_series)
     return L_average
 
 
@@ -196,9 +194,9 @@ def Dq(signal, kmax, fs):
     k = np.arange(1, kmax + 1)
     L = calc_L_average_series(k).astype(np.float64)
 
-    D, _ = -1*np.polyfit(np.log2(k), np.log2(L), 1)
+    D_t, _ = -1*np.polyfit(np.log2(k), np.log2(L), 1)
 
-    return D
+    return D_t
 
 
 def TSD_plot(dico_lead, name_lead, fs):
@@ -224,7 +222,7 @@ def TSD_plot(dico_lead, name_lead, fs):
 
     for i in name_lead:
         plt.figure()
-        fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(15, 15))
+        _, ax = plt.subplots(nrows=2, ncols=1, figsize=(15, 15))
         w_length = range(len(D_lead[i]))
         ax[0].plot(w_length, D_lead[i], label=i)
         ax[0].set_title(f"TSD time Evolution of Lead {i.decode('utf8')}")
@@ -238,12 +236,14 @@ def TSD_plot(dico_lead, name_lead, fs):
         ax[1].grid()
         plt.show()
 
-def TSD_mean_calculator(signal,segment_length,fs,k=1):
-
-    X = np.c_[[signal[int((w - 1)): int((w)+segment_length)] for w in range(1, int(len(signal)-segment_length),k)]]
-    L1 = np.array([Lq_k(X[i, :], 1, fs) for i in range(X.shape[0])])
-    L2 = np.array([Lq_k(X[i, :], 2,fs) for i in range(X.shape[0])])
-    Ds = (np.log(L1) - np.log(L2)) / (np.log(2))
+@njit
+def TSD_mean_calculator(signal2,segment_length,fs):
+    Ds = np.zeros(int(len(signal2)-segment_length))
+    for w in range(1,int(len(signal2)-segment_length)):
+        sig_true = signal2[int((w - 1)): int((w)+segment_length)]
+        L1 = Lq_k(sig_true, 1, fs)
+        L2 = Lq_k(sig_true,2,fs)
+        Ds[w] = (np.log(L1) - np.log(L2)) / (np.log(2))
     return np.mean(Ds), np.std(Ds)
 
 
@@ -258,7 +258,7 @@ def add_observational_noise(sig, SNR):
     return sig_noisy
 
 
-def TSDvsNoiseLevel_array(noise_level, path_to_data, list_attractor=["lorenz", "rossler"]):
+def TSDvsNoiseLevel_array(noise_level, path_to_data, list_attractor):
     Dmean = {}
     SD_D = {}
 
