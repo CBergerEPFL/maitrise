ce2148bca473514d66d5069f8f551ab06d41a943
hturbe <hugues.turbe@gmail.com>
2022-11-29 13:40:26 +0000

    reformat code

--- src/Metrics/Wrapper_main_function.py
+++ src/Metrics/Wrapper_main_function.py
@@ -1,38 +1,103 @@
 import os
 import sys
+
 import numpy as np
+import xarray as xr
+from tqdm import tqdm
+
 sys.path.append(os.path.join(os.getcwd(), ".."))
-from Metrics import Fiducial_metrics,Non_Fiducial_metrics,TSD_cal,Our_SQA_method
+from Metrics import (Fiducial_metrics, Non_Fiducial_metrics, Our_SQA_method,
+                     TSD_cal)
+
+dict_functions = {
+    "Corr_interlead": Fiducial_metrics.Corr_lead_score,
+    "Corr_intralead": Fiducial_metrics.Morph_score,
+    "wPMF": Non_Fiducial_metrics.wPMF_score,
+    "SNRECG": Non_Fiducial_metrics.SNR_index,
+    "HR": Fiducial_metrics.HR_index_calculator,
+    "Kurtosis": Fiducial_metrics.Kurto_score,
+    "Flatline": Fiducial_metrics.flatline_score,
+    "SDR": Non_Fiducial_metrics.SDR_score,
+    "TSD": TSD_cal.TSD_index,
+    "SQA": Our_SQA_method.SQA_method_score,
+}
+
+list_normalization = ["SNRECG", "TSD", "Flatline"]
 
 
-dict_functions =  {"Corr_interlead": Fiducial_metrics.Corr_lead_score,
-                   "Corr_intralead":Fiducial_metrics.Morph_score,
-                   "wPMF":Non_Fiducial_metrics.wPMF_score,
-                   "SNRECG" : Non_Fiducial_metrics.SNR_index,
-                   "HR":Fiducial_metrics.HR_index_calculator,
-                   "Kurtosis" : Fiducial_metrics.Kurto_score,
-                   "Flatline":Fiducial_metrics.flatline_score,
-                   "SDR":Non_Fiducial_metrics.SDR_score,
-                   "TSD" : TSD_cal.TSD_index,
-                   "SQA" : Our_SQA_method.SQA_method_score}
+def compute_metrics(
+    signal: np.ndarray, fs: np.ndarray, list_methods: list, normalization=True, verbose=False
+):
+    """Wrapper function to calculate metrics.
 
-list_normalization = ["SNRECG","TSD","Flatline"]
+    Args:
+        signal (np.array): Input signal.
+        fs (int): Sampling frequency.
+        list_methods (list): List of methods to calculate.
+        normalization (bool): Whether to normalize the metrics.
 
-def Wrapper(signals,name_method,fs):
-    if name_method not in list(dict_functions.keys()):
-        raise ValueError(f"The feature {name_method} is not implemented!")
-    elif name_method in list_normalization:
-        return dict_functions[name_method](signals,fs,normalization = True)
-    else :
-        return dict_functions[name_method](signals,fs)
+    Returns:
+        dict_metrics (dict): Dictionary of metrics.
+    """
 
-def main(signals,fs,list_methods):
-    ##You must give :
-    ##1) the ECG leads signals (numpy array [n_leads*len_signals])
-    ##2) Sampling frequency (only metadata required)
-    ##3) List of method names you want to use
-    X = np.zeros([signals.shape[0],len(list_methods)])
-    for i in range(len(list_methods)):
-        X[:,i] = Wrapper(signals,list_methods[i],fs)
+    if signal.ndim == 2:
+        signal = signal[np.newaxis, ...]
+
+    signal = np.transpose(signal, (0, 2, 1))
+    X = np.zeros([signal.shape[0], signal.shape[1], len(list_methods)])
+    for idx_method, name_method in enumerate(list_methods):
+        if name_method not in list(dict_functions.keys()):
+            raise ValueError(f"The feature {name_method} is not implemented!")
+        func_method = dict_functions[name_method]
+
+        for idx_signal in tqdm(
+            range(signal.shape[0]),
+            desc=f"Calculating {name_method}",
+            disable=not verbose,
+        ):
+            if name_method in list_normalization:
+                X[idx_signal, :, idx_method] = func_method(
+                    signal[idx_signal, ...], fs[idx_signal], normalization=normalization
+                )
+            else:
+                X[idx_signal, :, idx_method] = func_method(
+                    signal[idx_signal, ...], fs[idx_signal]
+                )
 
     return X
+
+
+def save_metrics_to_xarray(ds_data: xr.Dataset, list_methods: list, save_path: str, verbose=False):
+    """Compute and save metrics to xarray format.
+
+    Args:
+        ds_data (xarray): Data in xarray format.
+        list_methods (list): List of methods to calculate.
+        save_path (str): Path to save data.
+        verbose (bool): Whether to show progress bar.
+
+    Returns:
+        ds_data (xarray): Initial xarray merged with requested metrics in xarray format.
+    """
+
+    if (save_path is not None) and (not os.path.exists(save_path)):
+        os.makedirs(save_path)
+
+    signal = ds_data.signal.values
+    fs = ds_data.fs.values
+    # name_method = ["Corr_interlead","Corr_intralead","wPMF","SNRECG","HR","Kurtosis","Flatline","TSD"]
+    # Compute metrics
+    np_metrics = compute_metrics(
+        signal, fs, list_methods, normalization=True, verbose=verbose
+    )
+    da_metric = xr.DataArray(
+        np_metrics,
+        dims=["id", "lead_names", "metric_name"],
+        coords=[ds_data.id, ds_data.lead_names, list_methods],
+    )
+    ds_data["quality_metrics"] = da_metric
+
+    if save_path is not None:
+        ds_data.to_netcdf(os.path.join(save_path, "quality_metrics.nc"))
+
+    return ds_data
---
+++ src/shared_utils/utils_data.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import xarray as xr
+from petastorm import make_reader
+from tqdm import tqdm
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+def format_data_to_xarray(data_path: str, save_path: str | None = None):
+    """Format data to xarray format.
+
+    Args:
+        data_path (str): Path to data.
+        save_path (str): Path to save data. If save_path is not None, data will be saved to save_path.
+
+    Returns:
+        ds_ecg (xarray): Data in xarray format.
+    """
+    if (save_path is not None) and (not os.path.exists(save_path)):
+        os.makedirs(save_path)
+
+    if "file://" not in data_path:
+        path_petastorm = f"file:///{data_path}"
+    else:
+        path_petastorm = data_path
+    # Load data
+    array_signal = []
+    array_name = []
+    array_quality = []
+    array_fs = []
+    array_sex = []
+    with make_reader(path_petastorm) as reader:
+        for idx, sample in enumerate(reader):
+            if idx == 0:
+                lead_names = sample.signal_names.astype(str)
+            array_signal.append(sample.signal)
+            array_name.append(sample.noun_id.decode("utf-8"))
+            array_quality.append(sample.signal_quality.decode("utf-8"))
+            array_fs.append(sample.sampling_frequency)
+            array_sex.append(sample.sex.decode("utf-8"))
+
+    ds_ecg = xr.Dataset(
+        data_vars=dict(
+            signal=(["id", "time", "lead_name"], np.array(array_signal)),
+            data_quality=(["id"], np.array(array_quality)),
+            fs=(["id"], np.array(array_fs)),
+            sex=(["id"], np.array(array_sex)),
+        ),
+        coords=dict(
+            id=(["id"], np.array(array_name)),
+            time=(["time"], np.arange(0, 5000)),
+            lead_names=(["lead_names"], lead_names),
+        ),
+        attrs=dict(description="ecg with quality description"),
+    )
+
+    if save_path is not None:
+        ds_ecg.to_netcdf(os.path.join(save_path,"ecg_data.nc"))
+
+    return ds_ecg
